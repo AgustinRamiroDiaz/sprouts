26fe50e19f8d1740230a9c0bbc6d2f24c991ff39
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2025-02-01 20:05:02 -0300

    make example work + trunk
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,33 @@ use bevy::{asset::AssetMetaCheck, prelude::*};
 fn main() {
     let mut app = App::new();
     app.add_plugins((
-        DefaultPlugins,
-        // .set(AssetPlugin {
-        //     meta_check: AssetMetaCheck::Never, // https://github.com/bevyengine/bevy/issues/10157#issuecomment-2217168402
-        //     ..default()
-        // })
-        // .set(ImagePlugin::default_nearest())
-        // .set(WindowPlugin {
-        //     primary_window: Some(Window {
-        //         // Borderless looks distorted when running in the web
-        //         #[cfg(not(target_arch = "wasm32"))]
-        //         mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
-        //         fit_canvas_to_parent: true,
-        //         prevent_default_event_handling: false,
-        //         resizable: true,
-        //         ..default()
-        //     }),
-        //     ..default()
-        // }),
+        DefaultPlugins
+            .set(AssetPlugin {
+                meta_check: AssetMetaCheck::Never, // https://github.com/bevyengine/bevy/issues/10157#issuecomment-2217168402
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest())
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    // Borderless looks distorted when running in the web
+                    #[cfg(not(target_arch = "wasm32"))]
+                    mode: WindowMode::BorderlessFullscreen(MonitorSelection::Current),
+                    fit_canvas_to_parent: true,
+                    prevent_default_event_handling: false,
+                    resizable: true,
+                    ..default()
+                }),
+                ..default()
+            }),
         PhysicsPlugins::default(),
     ));
 
-    app.add_plugins((
-        DefaultPlugins,
-        // ExampleCommonPlugin,
-        PhysicsPlugins::default(),
-    ))
-    .insert_resource(ClearColor(Color::srgb(0.05, 0.05, 0.1)))
-    .insert_resource(SubstepCount(50))
-    .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
-    .add_systems(Startup, setup)
-    .add_systems(Update, follow_mouse)
-    .run();
+    app.insert_resource(ClearColor(Color::srgb(0.05, 0.05, 0.1)))
+        .insert_resource(SubstepCount(50))
+        .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
+        .add_systems(Startup, setup)
+        .add_systems(Update, follow_mouse)
+        .run();
 
     app.run();
 }
@@ -51,10 +46,12 @@ fn setup(
 ) {
     commands.spawn(Camera2d);
 
-    let particle_count = 100;
+    let particle_count = 50;
     let particle_radius = 1.2;
     let particle_mesh = meshes.add(Circle::new(particle_radius as f32));
     let particle_material = materials.add(Color::srgb(0.2, 0.7, 0.9));
+    let particle_mass = 1.0;
+    let compliance = 0.0000001; // TODO: explore this. It seems to be important
 
     // Spawn kinematic particle that can follow the mouse
     let mut previous_particle = commands
@@ -71,7 +68,10 @@ fn setup(
         let current_particle = commands
             .spawn((
                 RigidBody::Dynamic,
-                MassPropertiesBundle::from_shape(&Circle::new(particle_radius as f32), 1.0),
+                MassPropertiesBundle::from_shape(
+                    &Circle::new(particle_radius as f32),
+                    particle_mass,
+                ),
                 Mesh2d(particle_mesh.clone()),
                 MeshMaterial2d(particle_material.clone()),
                 Transform::from_xyz(0.0, -i as f32 * (particle_radius as f32 * 2.0 + 1.0), 0.0),
@@ -81,11 +81,27 @@ fn setup(
         commands.spawn(
             RevoluteJoint::new(previous_particle, current_particle)
                 .with_local_anchor_2(Vector::Y * (particle_radius * 2.0 + 1.0))
-                .with_compliance(0.0000001),
+                .with_compliance(compliance),
         );
 
         previous_particle = current_particle;
     }
+
+    // Spawn the fixed anchor and the last joint
+    let anchor_particle = commands
+        .spawn((
+            RigidBody::Static,
+            Transform::from_xyz(0.0, 0.0, 0.0),
+            Mesh2d(meshes.add(Circle::new(particle_radius * 2.0))),
+            MeshMaterial2d(materials.add(Color::srgb(0.2, 0.7, 0.9))),
+        ))
+        .id();
+
+    commands.spawn(
+        RevoluteJoint::new(previous_particle, anchor_particle)
+            .with_local_anchor_2(Vector::Y * (particle_radius * 2.0 + 1.0))
+            .with_compliance(compliance),
+    );
 }
 
 fn follow_mouse(
